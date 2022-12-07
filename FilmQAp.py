76e8abdd3e5bc5b1845e05311447336cd570f27c
csarux <cesaro02@ucm.es>
2022-12-07 15:01:32 +0100

    FilmQAp main page and image labeling simplified

--- FilmQAp.py
+++ FilmQAp.py
@@ -5,7 +5,7 @@ st.title('Dosimetría fotográfica con películas radiocrómicas')
 
 st.markdown(
 '''
-Aplicación para procesar películas radiocrómicas mediante un esquema de digitalización única (*SSP, single scan protocol*) leyéndose la película en la misma digitalización en la que se calibra el escáner. Si en un corto intervalo de tiempo se irradian las películas de medida y de calibración, el procedimiento es independiente del tiempo de evolución de la película, evitando tener que esperar el tiempo de estabilización. El procesado utiliza un análisis multicanal mediante promedios no locales. La calibración se realiza mediante un modelo sensitométrico de crecimiento con la dosis de dos fases del polímero presente en la capa sensible de la película. Las condiciones particulares que resultan del proceso de fabricación del lote, su conservación, el fondo y el estado del escáner se determinan en la digitalización. El algoritmo corrige el efecto lateral del escáner.
+**FilmQAp** es una aplicación para procesar películas radiocrómicas mediante un esquema de digitalización única (*SSP, single scan protocol*) leyéndose la película en la misma digitalización en la que se calibra el escáner. Si en un corto intervalo de tiempo se irradian las películas de medida y de calibración, el procedimiento es independiente del tiempo de evolución de la película, evitando tener que esperar el tiempo de estabilización. El procesado utiliza un análisis multicanal mediante promedios no locales. La calibración se realiza mediante un modelo sensitométrico de crecimiento con la dosis de dos fases del polímero presente en la capa sensible de la película. Las condiciones particulares que resultan del proceso de fabricación del lote, su conservación, el fondo y el estado del escáner se determinan en la digitalización. El algoritmo corrige el efecto lateral del escáner.
 
 La aplicación produce los archivos de dosis en formato `dxf` (formato de intercambio de Varian) que se pueden analizar mediante *Portal Dosimetry*.
 '''
@@ -17,5 +17,21 @@ st.markdown(
 '''     | **Página** | **Acciones** |
         |----------|---------|
         | Plano dosis planificador       | Procesar el plano de dosis calculado en el planificador (formato `DICOM`). Convertirlo a formato `dxf`       |
-        | Plano dosis película           | Importar la imagen digitalizada en formato `TIFF` (16 bits por canal). Etiquetar en la imagen digitalizada las regiones relevantes para aplicar el *SSP*. Determinar la dosis en la película. Reorientar el plano y exportar la distribución espacial de dosis medida en formato `dxf`| '''
+        | Plano dosis película           | Importar la imagen digitalizada en formato `TIFF` (16 bits por canal). Etiquetar en la imagen digitalizada las regiones relevantes para aplicar el *SSP*. Determinar la dosis en la película.| 
+        | Reorientar y exportar dosis    | Comparar la distribución epacial del plano de dosis calculado con la del medido para hacer coincidir su orientación. Exportar la distribución en formato `dxf`.| '''
+)
+
+st.subheader('Utilidades')
+
+st.markdown(
+'''
+        **FilmQAp** ofrece utilidades adicionales que complementan su funcionalidad'''
+)
+
+st.markdown(
+'''     | **Página** | **Acciones** |
+        |----------|--------|
+        | Análisis          | Analizar la coincidencia entre las distribuciones medidas y calculadas de forma autónoma mediante diferencias de dosis y mapas de índice gamma (*experimental*).|
+        | Calibración       | Mostrar las ajustes de calibración actualizados mediante el *SSP*. Valorar las incertidumbres de medida. | 
+        | Configuración     | Mostrar los parámetros de configuración de la aplicación. | '''
 )
---
+++ pages/22_Plano_dosis_película.py
@@ -0,0 +1,117 @@
+import os
+import pydicom as dicom
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import seaborn as sns
+import pyfilmqa as fqa
+from skimage.io import imread, imsave
+from skimage import img_as_ubyte
+from skimage.exposure import rescale_intensity
+from streamlit_img_label.manage import ImageManager, ImageDirManager
+from streamlit_img_label import st_img_label
+# - Config file
+import configparser
+import streamlit as st
+st.set_page_config(page_title='FilmQAp')
+
+
+def run(img_path, labels):
+    st.set_option("deprecation.showfileUploaderEncoding", False)
+    # Main content: annotate images
+    st.subheader('Etiquetar regiones')
+    im = ImageManager(img_path)
+    img = im.get_img()
+    resized_img = im.resizing_img()
+    resized_rects = im.get_resized_rects()
+    rects = st_img_label(resized_img, box_color="red", rects=resized_rects)
+
+    def process():
+        imfile = 'img_dir/' + scim.name
+        im.save_annotation()
+        image_annotate_file_name = scim.name.split(".")[0] + ".xml"
+     
+        lbdf = pd.DataFrame(im._current_rects)
+        lbdf.to_csv('tmp/bb.csv')
+
+        # Segmentar la imagen
+        fqa.segRegs(imfile=imfile)
+
+        # Leer la configuración
+        config = configparser.ConfigParser()
+        configfile='config/filmQAp.config'
+        config.read(configfile)
+
+        if 'Dmax' not in st.session_state:
+            st.error('Error: Plano de dosis calculado en el planificador no introducido, no es posible completar el postprocesado de la dosis medida por la película.')
+        else:
+        # Determinar las coordenadas para la corrección lateral
+            cdf = fqa.coordOAC(imfile=imfile)
+            # Determinación del fondo
+            abase = fqa.baseDetermination(imfile=imfile, config=config)
+            # Calibración de la digitalización
+            caldf, cddf, fps = fqa.PDDCalibration(config=config, imfile=imfile, base=abase)
+            # Incorporar al estado de la aplicación
+            if caldf not in st.session_state:
+                st.session_state.caldf = caldf
+            if cddf not in st.session_state:
+                st.session_state.cddf = cddf
+            if fps not in st.session_state:
+                st.session_state.fps = fps
+            
+            # Determinación de la dosis en cada canal
+            Dim = fqa.mphspcnlmprocf_multiprocessing(imfile=imfile, config=config, caldf=caldf, ccdf=cdf)
+
+            st.session_state.fDim = fqa.postmphspcnlmprocf(Dim, config=config)
+            if 'fDim' in st.session_state:
+                fcols, frows, fchannels = Dim.shape
+                st.session_state.fcols = fcols
+                st.session_state.frows = frows
+                st.success('Convertida a dosis la información de la película.')
+    
+    if rects:
+        with st.sidebar:
+            st.caption('Imagen digitalizada:')
+            st.button(label="Procesar", on_click=process)
+
+        preview_imgs = im.init_annotation(rects)
+
+        for i, prev_img in enumerate(preview_imgs):
+            prev_img[0].thumbnail((200, 200))
+            col1, col2 = st.columns(2)
+            with col1:
+                col1.image(prev_img[0])
+            with col2:
+                default_index = 0
+                if prev_img[1]:
+                    default_index = labels.index(prev_img[1])
+
+                select_label = col2.selectbox(
+                    "Label", labels, key=f"label_{i}", index=default_index
+                )
+                im.set_annotation(i, select_label)
+
+if __name__ == "__main__":
+    st.header('2. Procesar el plano de dosis medido mediante la película')
+
+    scim = st.file_uploader('Digitalización de la película:', help='Seleccionar el archivo TIFF adquirido en el escáner.')
+
+    if scim is not None:
+        # Guardar una copia local de la imagen
+        lcscim = os.path.join("img_dir",scim.name)
+        with open(lcscim,"wb") as f:
+            f.write(scim.getbuffer())
+        # Crear un registro en el estado de la alicación con el nombre del archivo de imagen
+        if 'FilmFileName' not in st.session_state:
+            st.session_state.FilmFileName = scim.name
+
+        # Guardar una copia local en formato png
+        aim = imread(lcscim)
+        imsave(os.path.join("img_dir",'Film.png'), img_as_ubyte(rescale_intensity(aim, out_range='uint8')))
+
+        # Etiquetar la imagen
+        custom_labels = ["", "Film", "Calibration", "Background", "Center"]
+
+        run('img_dir/Film.png', custom_labels)
