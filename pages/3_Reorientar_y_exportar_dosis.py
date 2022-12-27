8ad044f4b01826a8e3f63dd2aca472fbac69b2c6
csarux <77076761+csarux@users.noreply.github.com>
2022-12-27 08:16:01 +0100

    Film dxf string
    Modificada tif2dxf. Añadido el botón para descargar Film.dxf. Eliminada las páginas 22_Plano_dosis_película y 10_analisisDocumentacion

--- pages/3_Reorientar_y_exportar_dosis.py
+++ pages/3_Reorientar_y_exportar_dosis.py
@@ -16,42 +16,50 @@ import streamlit as st
 
 def original():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
-    tif2dxf(fDim=fDim)
+    dxffilmstr = tif2dxfString(fDim=fDim)
+    return dxffilmstr
 
 def rot90():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.rot90(fDim)
-    tif2dxf(fDim=fDim)
+    dxffilmstr = tif2dxfString(fDim=fDim)
+    return dxffilmstr
 
 def rot180():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.rot90(fDim, k=2)
-    tif2dxf(fDim=fDim)
+    dxffilmstr = tif2dxfString(fDim=fDim)
+    return dxffilmstr
 
 def rot270():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.rot90(fDim, k=3)
-    tif2dxf(fDim=fDim)
+    dxffilmstr = tif2dxfString(fDim=fDim)
+    return dxffilmstr
 
 def voltear():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.fliplr(fDim)
-    tif2dxf(fDim=fDim)
+    dxffilmstr = tif2dxfString(fDim=fDim)
+    return dxffilmstr
 
 def vrot90():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.rot90(np.fliplr(fDim))
-    tif2dxf(fDim=fDim)
+    dxffilmstr = tif2dxfString(fDim=fDim)
+    return dxffilmstr
 
 def vrot180():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.rot90(np.fliplr(fDim), k=2)
-    tif2dxf(fDim=fDim)
+    dxffilmstr = tif2dxfString(fDim=fDim)
+    return dxffilmstr
 
 def vrot270():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.rot90(np.fliplr(fDim), k=3)
-    tif2dxf(fDim=fDim)
+    dxffilmstr = tif2dxfString(fDim=fDim)
+    return dxffilmstr
 
 def tif2dxf(fDim=None):
     pxsp = fqa.TIFFPixelSpacing(os.path.join("img_dir", st.session_state.FilmFileName))
@@ -75,6 +83,23 @@ def tif2dxf(fDim=None):
                   PatientId2=st.session_state.PatientId, LastName=st.session_state.LastName,
                   FirstName=st.session_state.FirstName, pxsp=pxsp, imsz=imsz)
     st.success('Exportada la dosis medida por la película en formato dxf: \n' + config['DEFAULT']['exportpath'] + st.session_state.PatientId + '/StremlitFilm.dxf')
+
+def tif2dxfString(fDim=None):
+    pxsp = fqa.TIFFPixelSpacing(os.path.join("img_dir", st.session_state.FilmFileName))
+    imsz = fqa.DoseImageSize(fDim)
+
+    if 'fDdf' not in st.session_state:
+        ya = np.linspace(0, (imsz[0]-1)*pxsp[0], imsz[0])
+        xa = np.linspace(0, (imsz[1]-1)*pxsp[1], imsz[1])
+        fDdf = pd.DataFrame(data=np.transpose(fDim), index=ya, columns=xa) # Impuesto por la manera en la que Eclipse orienta el plano de dosis en Portal Dosimetry
+        st.session_state.fDdf = fDdf
+    if 'fps' not in st.session_state:
+        st.session_state.fps = pxsp
+
+    dxfstr = fqa.dxfString(Data=fDim, AcqType='Acquired Portal', PatientId1=st.session_state.PatientId,
+                           PatientId2=st.session_state.PatientId, LastName=st.session_state.LastName,
+                           FirstName=st.session_state.FirstName, pxsp=pxsp, imsz=imsz)
+    return dxfstr
 
 st.title('3. Reorientar con respecto al planificador y exportar la dosis medida por la película')
 
@@ -106,18 +131,23 @@ else:
     imSel = image_select("Seleccionar la orientación", [imOrig, imR90, imR180, imR270, imV, imVR90, imVR180, imVR270], use_container_width=False, return_value='index')
 
     if imSel == 0:
-        original()
+        dxffilmstr = original()
     elif imSel == 1:
-        rot90()
+        dxffilmstr = rot90()
     elif imSel == 2:
-        rot180()
+        dxffilmstr = rot180()
     elif imSel == 3:
-        rot270()
+        dxffilmstr = rot270()
     elif imSel == 4:
-        voltear()
+        dxffilmstr = voltear()
     elif imSel == 5:
-        vrot90()
+        dxffilmstr = vrot90()
     elif imSel == 6:
-        vrot180()
+        dxffilmstr = vrot180()
     elif imSel == 7:
-        vrot270()
+        dxffilmstr = vrot270()
+
+    if dxffilmstr:
+        with st.sidebar:
+            st.download_button(label='Descargar dxf', data=dxffilmstr, file_name='Film.dxf', mime='text/csv')
+
