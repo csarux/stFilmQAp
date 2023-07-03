42202a0fe56983813460cfb895b1aabacf67be5d
csarux <77076761+csarux@users.noreply.github.com>
2023-07-03 18:47:59 +0200

    Update 3_Calibración.py

--- pages/3_Calibración.py
+++ pages/3_Calibración.py
@@ -80,7 +80,7 @@ with colr:
 
 with colh:
     fig, ax = plt.subplots()
-    ax.hist(cddf['uDr_%'], bins=15)
+    ax.hist(cddf['uDr_%'], bins=20)
 
     ax.set_xlabel('Residuo relativo [%]')
     ax.set_ylabel('Cuentas')
