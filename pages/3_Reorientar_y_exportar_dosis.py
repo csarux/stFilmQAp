fd8d325f3b023f9b8588e3617695f4b2a34a7e1e
csarux <cesaro02@ucm.es>
2023-10-15 17:58:34 +0200

    Update 3_Reorientar_y_exportar_dosis.py

--- pages/3_Reorientar_y_exportar_dosis.py
+++ pages/3_Reorientar_y_exportar_dosis.py
@@ -16,76 +16,44 @@ import streamlit as st
 
 def original():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
-    dxffilmstr = tif2dxfString(fDim=fDim)
-    return dxffilmstr
+    st.session_state.fOrDim = fDim
 
 def rot90():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.rot90(fDim)
-    dxffilmstr = tif2dxfString(fDim=fDim)
-    return dxffilmstr
+    st.session_state.fOrDim = fDim
 
 def rot180():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.rot90(fDim, k=2)
-    dxffilmstr = tif2dxfString(fDim=fDim)
-    return dxffilmstr
+    st.session_state.fOrDim = fDim
 
 def rot270():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.rot90(fDim, k=3)
-    dxffilmstr = tif2dxfString(fDim=fDim)
-    return dxffilmstr
+    st.session_state.fOrDim = fDim
 
 def voltear():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.fliplr(fDim)
-    dxffilmstr = tif2dxfString(fDim=fDim)
-    return dxffilmstr
+    st.session_state.fOrDim = fDim
 
 def vrot90():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.rot90(np.fliplr(fDim))
-    dxffilmstr = tif2dxfString(fDim=fDim)
-    return dxffilmstr
+    st.session_state.fOrDim = fDim
 
 def vrot180():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.rot90(np.fliplr(fDim), k=2)
-    dxffilmstr = tif2dxfString(fDim=fDim)
-    return dxffilmstr
+    st.session_state.fOrDim = fDim
 
 def vrot270():
     fDim = np.array(st.session_state.fDim).reshape((st.session_state.fcols, st.session_state.frows))
     fDim = np.rot90(np.fliplr(fDim), k=3)
-    dxffilmstr = tif2dxfString(fDim=fDim)
-    return dxffilmstr
+    st.session_state.fOrDim = fDim
 
 def tif2dxf(fDim=None):
-    pxsp = fqa.TIFFPixelSpacing(os.path.join("img_dir", st.session_state.FilmFileName))
-    imsz = fqa.DoseImageSize(fDim)
-
-    if 'fDdf' not in st.session_state:
-        ya = np.linspace(0, (imsz[0]-1)*pxsp[0], imsz[0])
-        xa = np.linspace(0, (imsz[1]-1)*pxsp[1], imsz[1])
-#        fDdf = pd.DataFrame(data=np.transpose(fDim), index=ya, columns=xa) # Impuesto por la manera en la que Eclipse orienta el plano de dosis en Portal Dosimetry
-        fDdf = pd.DataFrame(data=fDim, index=xa, columns=ya)
-        st.session_state.fDdf = fDdf
-    if 'fps' not in st.session_state:
-        st.session_state.fps = pxsp
-
-    config = configparser.ConfigParser()
-    configfile='config/filmQAp.config'
-    config.read(configfile)
-    dxffilePath = Path(config['DEFAULT']['exportpath'] + st.session_state.PatientId + '/StreamlitFilm.dxf')
-
-    fqa.dxfWriter(Data=fDim, dxfFileName=dxffilePath,
-                  AcqType='Acquired Portal', PatientId1=st.session_state.PatientId,
-                  PatientId2=st.session_state.PatientId, LastName=st.session_state.LastName,
-                  FirstName=st.session_state.FirstName, pxsp=pxsp, imsz=imsz)
-    st.success('Exportada la dosis medida por la película en formato dxf: \n' + config['DEFAULT']['exportpath'] + st.session_state.PatientId + '/StremlitFilm.dxf')
-
-def tif2dxfString(fDim=None):
     pxsp = fqa.TIFFPixelSpacing(os.path.join("img_dir", st.session_state.FilmFileName))
     imsz = fqa.DoseImageSize(fDim)
 
@@ -97,25 +65,76 @@ def tif2dxfString(fDim=None):
     if 'fps' not in st.session_state:
         st.session_state.fps = pxsp
 
-    dxfstr = fqa.dxfString(Data=fDim, AcqType='Acquired Portal', PatientId1=st.session_state.PatientId,
-                           PatientId2=st.session_state.PatientId, LastName=st.session_state.LastName,
-                           FirstName=st.session_state.FirstName, pxsp=pxsp, imsz=imsz)
-    return dxfstr
+    config = configparser.ConfigParser()
+    configfile='config/filmQAp.config'
+    config.read(configfile)
+    if 'PatientId' not in st.session_state:
+        st.session_state.PatientId = config['Demographics']['patientid']
+    if 'FirstName' not in st.session_state:
+        st.session_state.FirstName = config['Demographics']['patientname']
+    if 'LastName' not in st.session_state:
+        st.session_state.LastName = config['Demographics']['patientfamilyname']
+    if 'Gender' not in st.session_state:
+        st.session_state.Gender = config['Demographics']['gender']
+    if 'DoseFilmFileName' not in st.session_state:
+        st.session_state.DoseFilmFileName = config['FilmPlane']['filename']
+    
+    dxffilePath = Path(config['DEFAULT']['exportpath'] + st.session_state.DoseFilmFileName + '.dxf')
 
-st.title('3. Reorientar con respecto al planificador y exportar la dosis medida por la película')
+    fqa.dxfWriter(Data=fDim, dxfFileName=dxffilePath,
+                  AcqType='Acquired Portal', PatientId1=st.session_state.PatientId,
+                  PatientId2=st.session_state.PatientId, LastName=st.session_state.LastName,
+                  FirstName=st.session_state.FirstName, pxsp=pxsp, imsz=imsz)
 
+def tif2dxfString(fDim=None, config=None):
+    pxsp = fqa.TIFFPixelSpacing(os.path.join("img_dir", st.session_state.FilmFileName))
+    imsz = fqa.DoseImageSize(fDim)
+
+    ya = np.linspace(0, (imsz[0]-1)*pxsp[0], imsz[0])
+    xa = np.linspace(0, (imsz[1]-1)*pxsp[1], imsz[1])
+    fDdf = pd.DataFrame(data=np.transpose(fDim), index=ya, columns=xa) # Impuesto por la manera en la que Eclipse orienta el plano de dosis en Portal Dosimetry
+    st.session_state.fDdf = fDdf
+    if 'fps' not in st.session_state:
+        st.session_state.fps = pxsp
+
+    if 'PatientId' not in st.session_state:
+        st.session_state.PatientId = config['Demographics']['patientid']
+    if 'FirstName' not in st.session_state:
+        st.session_state.FirstName = config['Demographics']['patientname']
+    if 'LastName' not in st.session_state:
+        st.session_state.LastName = config['Demographics']['patientfamilyname']
+    if 'Gender' not in st.session_state:
+        st.session_state.Gender = config['Demographics']['gender']
+    if 'DoseFilmFileName' not in st.session_state:
+        st.session_state.DoseFilmFileName = config['FilmPlane']['filename']
+    
+    dxffilePath = Path(config['DEFAULT']['exportpath'] + st.session_state.DoseFilmFileName + '.dxf')
+
+    dxffilmstr = fqa.dxfString(Data=fDim,
+                               AcqType='Acquired Portal', PatientId1=st.session_state.PatientId,
+                               PatientId2=st.session_state.PatientId, LastName=st.session_state.LastName,
+                               FirstName=st.session_state.FirstName, pxsp=pxsp, imsz=imsz)
+    return dxffilmstr
+
+st.title('2. Reorientar y exportar')
+
+st.markdown(
+'''
+Seleccionar la orientación deseada de la imagen de dosis y exportar en el formato elegido
+'''
+)
 
 if 'pDdf' not in st.session_state:
     st.error('Error: Plano de dosis del planificador no importado.')
 else:
     imPlan = invert(img_as_ubyte(rescale_intensity(st.session_state.pDdf.values, out_range='uint8')))
-    st.subheader('Planificador')
+    st.subheader('Distribución de dosis calculada')
     st.image(imPlan)
 
 if 'fDim' not in st.session_state:
     st.error('Error: Plano de dosis de la película no procesado.')
 else:
-    st.subheader('Película')
+    st.subheader('Distribución de dosis medida')
 
     fDim = st.session_state.fDim
 
@@ -132,23 +151,32 @@ else:
     imSel = image_select("Seleccionar la orientación", [imOrig, imR90, imR180, imR270, imV, imVR90, imVR180, imVR270], use_container_width=False, return_value='index')
 
     if imSel == 0:
-        dxffilmstr = original()
+        original()
     elif imSel == 1:
-        dxffilmstr = rot90()
+        rot90()
     elif imSel == 2:
-        dxffilmstr = rot180()
+        rot180()
     elif imSel == 3:
-        dxffilmstr = rot270()
+        rot270()
     elif imSel == 4:
-        dxffilmstr = voltear()
+        voltear()
     elif imSel == 5:
-        dxffilmstr = vrot90()
+        vrot90()
     elif imSel == 6:
-        dxffilmstr = vrot180()
+        vrot180()
     elif imSel == 7:
-        dxffilmstr = vrot270()
+        vrot270()
+    
+    config = configparser.ConfigParser()
+    configfile='config/filmQAp.config'
+    config.read(configfile)
 
-    if dxffilmstr:
+    if 'fOrDim' in st.session_state:
+        dxffilmstr = tif2dxfString(st.session_state.fOrDim, config=config)
+        dcmfilmstr = fqa.dcmWriter(Data=st.session_state.fOrDim, imfile=Path('img_dir', st.session_state.FilmFileName), config=config)
+        dcmFileName = 'RD.' + fqa.getMediaStorageSOPInstanceUID(dcmfilmstr) +'.dcm'
         with st.sidebar:
             st.download_button(label='Descargar dxf', data=dxffilmstr, file_name='Film.dxf', mime='text/csv')
+            st.download_button(label='Descargar dcm', data=dcmfilmstr, file_name=dcmFileName)
+
 
